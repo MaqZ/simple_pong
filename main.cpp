cb0c85ba7150495ff88a1736e46c2de75313826a
MaqZ <maqusser@gmail.com>
2016-07-30 22:44:32 +0200

    Added alpha branch

--- main.cpp
+++ main.cpp
@@ -3,28 +3,6 @@
 #include "matchscreen.hpp"
 #include <SFML/Graphics.hpp>
 
-/*
-template<class T>
-void print_v2(std::vector<std::vector<T>> _v){
-  std::cout << "[";
-
-  for(unsigned i = 0; i < _v.size(); i++){
-    std::cout << "[";
-
-    for(unsigned j = 0; j < _v[i].size(); j++){
-      std::cout << _v[i][j];
-      if(j != _v.size() - 1)
-        std::cout << ", ";
-    }
-    std::cout << "]";
-    
-    if(i != _v.size() - 1)
-      std::cout << ", ";
-  }
-  std::cout << "]\n";
-}
-*/
-
 int main(){
   ScreenManager &scrn = ScreenManager::get_instance();
   sf::RenderWindow app(sf::VideoMode(800, 600), "Simple Pong", sf::Style::None);
